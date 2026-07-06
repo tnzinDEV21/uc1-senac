f7d4c41b1dd61961ce56fac0d0222d7321c1e35d
tnzinDEV21 <tony.github21@gmail.com>
2026-07-06 20:45:06 -0300

    prog26.py

---
+++ prog26.py
@@ -0,0 +1,6 @@
+v1 = int(input("digite o primeiro valor: "))
+v2 = int(input("digite o segundo valor: "))
+if v1 > v2 :
+    print(f"{v1} valor e o maior: ")
+else:
+    print(f"{v2} valor e o maior: ")
