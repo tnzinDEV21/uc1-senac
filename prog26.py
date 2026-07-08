fad591c4c46f7c4b00ed585264ad1d4f5c57c2ee
tnzinDEV21 <tony.github21@gmail.com>
2026-07-08 18:17:59 -0300

    Update prog26.py

--- prog26.py
+++ prog26.py
@@ -4,10 +4,3 @@ if v1 > v2 :
     print(f"{v1} valor e o maior: ")
 else:
     print(f"{v2} valor e o maior: ")
-<<<<<<< HEAD
-    print("teste de push pelo VScode")
-=======
-    print("haha")
->>>>>>> a4967cc7c8bb2c18611795642c050635b6b68507
-    print("brano yag")
-
